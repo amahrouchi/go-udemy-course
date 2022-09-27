d5448c8d3de5e01e2cac9745164b956e1484c687
Angelo Mahrouchi <angelo.mahrouchi@ankorstore.com>
2022-09-27 09:26:47 +0200

    feat(constants): comment about constants

---
+++ constants.go
@@ -0,0 +1,18 @@
+package main
+
+// Valeur définie à la compilation (en remplcant les occurrences de la constante directement par sa valeur)
+// Ne pourra donc pas être assigné pendant l'execution du programme à partir d'une variable
+// lowercase snake case
+
+// Déclaration
+// const refresh_delay = 500
+
+// Impossible
+// lang := "Go"
+// const language = lang
+// La déclatation dépend d'un valeur variable qui pourrait changer à l'execution
+// le compilateur ne peut donc pas définir la valeur de cette variable au moment de la compilation
+
+func main() {
+
+}
